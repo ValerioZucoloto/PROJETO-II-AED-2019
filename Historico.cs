6ebb6eb5c66d2d92477c9bc449ffa2771a37f542
Arley Pereira <47366406+ArleyPereira@users.noreply.github.com>
2019-12-05 21:16:11 -0300

    Add Passegeiro.cs, Voo.cs, Historico.cs

---
+++ Historico.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Historico {
+
+        public DateTime data { get; set; }
+        public Aviao aviao;
+        public Piloto piloto;
+
+        public static string CAMINHO_BANCO = "../";
+
+        public Historico() {
+
+        }
+
+        public static void GetHistorico(string cpf){
+
+        }
+
+        public static void Salvar(Voo voo){
+
+        }
+
+}
---
+++ Voo.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+
+class Voo {
+        public Piloto piloto;
+        public List<Passageiro> listaPassageiro = new List<Passageiro>();
+}
